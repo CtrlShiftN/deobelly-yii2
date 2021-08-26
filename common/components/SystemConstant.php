6506584bd8b40e88959f6be7aced526bc223733f
Doctor <karuyanakaido24@gmail.com>
2021-08-26 20:56:33 +0700

    add libs Signed-off-by: Doctor <karuyanakaido24@gmail.com>

---
+++ common/components/SystemConstant.php
@@ -0,0 +1,9 @@
+<?php
+namespace common\components;
+class SystemConstant
+{
+    // Api status
+    const API_SUCCESS_STATUS = 1;
+    const API_UNSUCCESS_STATUS = 0;
+    const LIMIT_PER_PAGE = 12;
+}
