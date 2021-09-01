946bcc0b5a29de71f550359096f368e81bf67f51
vuvuvuvvv <nguyenvu260502@gmail.com>
2021-09-01 19:29:35 +0700

    Signed-off-by: vuvuvuvvv <nguyenvu260502@gmail.com>

--- frontend/views/site/contact.php
+++ frontend/views/site/contact.php
@@ -18,11 +18,30 @@ $this->registerCssFile(Url::toRoute('css/contact.css'));
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.8752675235874!2d105.77944141269522!3d21.037676292854805!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454b55b011e49%3A0x9406c12dc4604160!2zxJDhuqFpIGjhu41jIFF14buRYyBnaWEgSMOgIE7hu5lp!5e0!3m2!1svi!2s!4v1629020641063!5m2!1svi!2s"
                 width="100%" height="500" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     </div>
+    <div class="row border-bottom">
+        <div class="p-3 text-center my-3">
+            <h5> Nếu bạn có yêu cầu kinh doanh hoặc câu hỏi khác, vui lòng điền vào biểu mẫu sau để liên hệ với chúng tôi. Cảm ơn bạn!</h5>
+        </div>
+    </div>
+    <?php if (Yii::$app->session->hasFlash('success')): ?>
+        <div class="alert alert-success alert-dismissible fade show">
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            <h4><i class="icon fa fa-check"></i> THÀNH CÔNG!</h4>
+            <?= Yii::$app->session->getFlash('success') ?>
+        </div>
+    <?php endif; ?>
+    <?php if (Yii::$app->session->hasFlash('error')): ?>
+        <div class="alert alert-danger alert-dismissible fade show">
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            <h4><i class="fas fa-times"></i> LỖI!</h4>
+            <?= Yii::$app->session->getFlash('error') ?>
+        </div>
+    <?php endif; ?>
     <div class="row mt-3 ">
-        <div class="col-lg-6 order-lg-first order-xs-last">
+        <div class="col-lg-6 order-lg-first order-xs-last my-3">
             <h3 class="fw-bold mb-4 text-center text-md-start"><span
                         class=" border-3 border-dark border-bottom">LIÊN HỆ</span></h3>
-            <div class="fs-5">
+            <div class="">
                 <p><span class="text-muted">Địa chỉ của chúng tôi</span><br>Tầng 1,tòa nhà GP Invest, 170 Đê La Thành,
                     Đống Đa,Hà Nội</p>
                 <p><span class="text-muted">Email của chúng tôi</span><br>hi@gmail.com</p>
@@ -32,42 +51,29 @@ $this->registerCssFile(Url::toRoute('css/contact.css'));
             </div>
         </div>
 
-        <div class="col-12 col-lg-6 order-sm-first order-xs-first">
-            <h3 class="fw-bold mb-4  text-center text-md-start"><span class=" border-3 border-dark border-bottom ">GỬI THẮC MẮC CỦA BẠN</span>
+        <div class="col-12 col-lg-6 order-sm-first order-xs-first my-3">
+            <h3 class="fw-bold mb-4 text-center"><span class=" border-3 border-dark border-bottom ">GỬI THẮC MẮC CỦA BẠN</span>
             </h3>
             <div class="hr"></div>
             <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
 
-            <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
+            <?= $form->field($model, 'name')->textInput(['autofocus' => true])->label('Họ và Tên:') ?>
 
             <div class="row">
-                <div class="col-md-6">
-                    <?= $form->field($model, 'email') ?>
+                <div class="col-md-6 my-3">
+                    <?= $form->field($model, 'email')->label('Địa chỉ mail:') ?>
                 </div>
-                <div class="col-md-6">
-                    <?= $form->field($model, 'tel') ?>
+                <div class="col-md-6 my-3">
+                    <?= $form->field($model, 'tel')->label('Số điện thoại:') ?>
                 </div>
             </div>
 
-            <?= $form->field($model, 'content')->textarea(['rows' => 6]) ?>
-            <div class="form-group">
-                <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
+            <?= $form->field($model, 'content')->textarea(['rows' => 6])->label('Nội dung liên hệ:')?>
+
+            <div class="form-group mt-2 mt-md-3">
+                <?= Html::submitButton('Gửi', ['class' => 'btn btn-dark p-2 col-12', 'name' => 'contact-button']) ?>
             </div>
             <?php ActiveForm::end(); ?>
         </div>
-        <?php if (Yii::$app->session->hasFlash('success')): ?>
-            <div class="alert alert-success alert-dismissable">
-                <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
-                <h4><i class="icon fa fa-check"></i> THÀNH CÔNG!</h4>
-                <?= Yii::$app->session->getFlash('success') ?>
-            </div>
-        <?php endif; ?>
-        <?php if (Yii::$app->session->hasFlash('error')): ?>
-            <div class="alert alert-danger alert-dismissable">
-                <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
-                <h4><i class="fas fa-times"></i> LỖI!</h4>
-                <?= Yii::$app->session->getFlash('error') ?>
-            </div>
-        <?php endif; ?>
     </div>
 </div>
