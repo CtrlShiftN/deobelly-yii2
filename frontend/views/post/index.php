f43ff01525362685e7cf4ac1073027a0fc28210a
Doctor <karuyanakaido24@gmail.com>
2021-09-17 19:15:42 +0700

    Add post tag Signed-off-by: Doctor <karuyanakaido24@gmail.com>

--- frontend/views/post/index.php
+++ frontend/views/post/index.php
@@ -11,63 +11,65 @@ use yii\widgets\LinkPager;
 
 $imgUrl = Yii::$app->params['common'] . "/media";
 ?>
-        <?php foreach ($post as $value) : ?>
-            <div class="col-12 row px-0 m-0 py-3 py-md-4">
-                <div class="col-4 px-md-5"><a href="#"><img src="<?= $imgUrl . '/' . $value['avatar'] ?>"
-                                                            class="img-fluid"></a>
-                </div>
-                <div class="col-8">
-                    <div class="h-75">
-                        <h4 class="tag-ellipsis text-uppercase fw-bold"><?= $value['title'] ?></h4>
-                        <i class="far fa-calendar-alt"><span
-                                    class="me-3"> <?= $value['updated_at'] ?></span></i>
-                        <i class="fas fa-user d-none d-sm-inline-block"><span
-                                    class="ms-1"><?= User::findOne(['id' => $value['admin_id']])['name']; ?></span></i>
-                        <div class="mt-1 mt-md-2 title">
-                            <?php if (!empty($value['tag_id'])): ?>
-                                <?php foreach (\frontend\models\PostTag::getPostTag(explode(',', $value['tag_id'])) as $tag) : ?>
-                                    <a target="_blank"
-                                       href="<?= Url::toRoute(['post/index', 'post_tag' => \common\components\encrypt\CryptHelper::encryptString($value['tag_id'])]) ?>"><span
-                                                class="badge border text-dark"><?= Yii::t('app', $tag['title']) ?></span></a>
-                                <?php endforeach; ?>
-                            <?php endif; ?>
-                        </div>
-                        <div class="short-text mt-2 fs-5 lh-sm"><?= $value['content'] ?></div>
-                    </div>
-                    <div class="h-25 watch-more ">
-                        <div class="d-flex align-items-end float-end h-100 fs-5">
-                            <a href="#"><span>Xem thêm <i class="fas fa-arrow-right"></i></span></a>
-                        </div>
-                    </div>
+<h2>Tin tức</h2>
+<div class="row pe-3">
+    <?php foreach ($post as $key => $value) : ?>
+        <!-- Three in a row -->
+
+        <div class="col-12 col-md-6 col-lg-4 py-3">
+            <div class="article-head">
+                <a href="<?= Url::toRoute(['post/detail', 'id' => \common\components\encrypt\CryptHelper::encryptString($value['id'])]) ?>"><img
+                            class="article-head__image"
+                            src="<?= $imgUrl . '/' . $value['avatar'] ?>"
+                            alt="<?= $value['slug'] ?>"></a>
+
+                <div class="article-meta d-flex justify-content-center align-items-center py-1">
+                    <i class="fa fa-calendar"></i>&nbsp;<b class="color_main"><?= date_format(date_create($value['created_at']), 'H:i:s d/m/Y') ?></b>
+                    &nbsp; Đăng bởi:&nbsp; <b class="color_main"><?= $value['name'] ?></b>
                 </div>
             </div>
-        <?php endforeach; ?>
-        <?= LinkPager::widget([
-            'pagination' => $pages,
-            //Css option for container
-            'options' => ['class' => 'list-unstyled d-flex align-items-center justify-content-center', 'id' => "pagination"],
-            //First option value
-            'firstPageLabel' => '&#10094;&#10094;',
-            //Last option value
-            'lastPageLabel' => '&#10095;&#10095;',
-            //Previous option value
-            'prevPageLabel' => '&#10094;',
-            //Next option value
-            'nextPageLabel' => '&#10095;',
-            //Current Active option value
-            'activePageCssClass' => 'bg-danger p-active',
-            //Max count of allowed options
-            'maxButtonCount' => 3,
-            // Css for each options. Links
-            'linkOptions' => ['class' => 'text-decoration-none'],
-            'disabledPageCssClass' => 'disabled p-2 border p-inactive',
-            // Customzing CSS class for navigating link
-            'prevPageCssClass' => 'p-back',
-            'nextPageCssClass' => 'p-next',
-            'firstPageCssClass' => 'p-first',
-            'lastPageCssClass' => 'p-last',
-        ])
-        ?>
+            <div class="article-title text-center">
+                <a class="text-decoration-none text-dark title-heading" href="<?= Url::toRoute(['post/detail', 'id' => \common\components\encrypt\CryptHelper::encryptString($value['id'])]) ?>"><h5><?= $value['title'] ?></h5></a>
+            </div>
+            <div class="article-content text-center text-justify">
+                <?= $value['content'] ?>
+            </div>
+            <ul class="tags mt-2">
+                <?php foreach (\frontend\models\PostTag::getPostTag(explode(',', $value['tag_id'])) as $tag) : ?>
+                <li><a target="_blank" class="tag"
+                       href="<?= Url::toRoute(['post/index', 'post_tag' => \common\components\encrypt\CryptHelper::encryptString($value['tag_id'])]) ?>">
+                                <?= Yii::t('app', $tag['title']) ?></a></li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    <?php endforeach; ?>
+</div>
+<?= LinkPager::widget([
+    'pagination' => $pages,
+    //Css option for container
+    'options' => ['class' => 'list-unstyled d-flex align-items-center justify-content-center', 'id' => "pagination"],
+    //First option value
+    'firstPageLabel' => '&#10094;&#10094;',
+    //Last option value
+    'lastPageLabel' => '&#10095;&#10095;',
+    //Previous option value
+    'prevPageLabel' => '&#10094;',
+    //Next option value
+    'nextPageLabel' => '&#10095;',
+    //Current Active option value
+    'activePageCssClass' => 'bg-danger p-active',
+    //Max count of allowed options
+    'maxButtonCount' => 3,
+    // Css for each options. Links
+    'linkOptions' => ['class' => 'text-decoration-none'],
+    'disabledPageCssClass' => 'disabled p-2 border p-inactive',
+    // Customzing CSS class for navigating link
+    'prevPageCssClass' => 'p-back',
+    'nextPageCssClass' => 'p-next',
+    'firstPageCssClass' => 'p-first',
+    'lastPageCssClass' => 'p-last',
+])
+?>
 <script>
     $("#pagination").children().addClass('p-2 px-3 h-100 border').children().addClass("text-dark");
 </script>
