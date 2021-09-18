f44b8603c32f3e55b5f2a5b9644cbbc238647824
Doctor <karuyanakaido24@gmail.com>
2021-09-18 17:24:58 +0700

    Add post tag modal to create new Signed-off-by: Doctor <karuyanakaido24@gmail.com>

--- backend/views/post-tag/_form.php
+++ backend/views/post-tag/_form.php
@@ -17,7 +17,7 @@ $this->title = Yii::t('app', 'Create new post tags');
     <?php $form = ActiveForm::begin(); ?>
 
     <div class="row pb-3">
-        <div class="col-12 col-sm-2 col-md-3 col-lg-3"><h4><?= Yii::t('app', 'Title') ?> <sup
+        <div class="col-12 col-sm-2 col-md-3 col-lg-4"><h4><?= Yii::t('app', 'Title') ?> <sup
                         class="fill-red fs-6">(*)</sup></h4></div>
         <div class="col-12 col-sm-6 col-md-5 col-lg-6">
             <?= $form->field($model, 'title')->textInput(['placeholder' => 'Thời trang thu đông'])->label(false) ?>
