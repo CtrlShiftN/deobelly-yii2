758cf5d772f7525cf50f4fafa3dca0e748ed7c48
Doctor <karuyanakaido24@gmail.com>
2021-10-11 12:54:33 +0700

    Signed-off-by: Doctor <karuyanakaido24@gmail.com>

---
+++ backend/views/product/_form.php
@@ -0,0 +1,67 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\Product */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="product-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'slug')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'short_description')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'cost_price')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'regular_price')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'sale_price')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'selling_price')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'SKU')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'quantity')->textInput() ?>
+
+    <?= $form->field($model, 'image')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'images')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'is_luxury')->textInput() ?>
+
+    <?= $form->field($model, 'related_product')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'gender')->textInput() ?>
+
+    <?= $form->field($model, 'trademark_id')->textInput() ?>
+
+    <?= $form->field($model, 'viewed')->textInput() ?>
+
+    <?= $form->field($model, 'fake_sold')->textInput() ?>
+
+    <?= $form->field($model, 'sold')->textInput() ?>
+
+    <?= $form->field($model, 'status')->textInput() ?>
+
+    <?= $form->field($model, 'admin_id')->textInput() ?>
+
+    <?= $form->field($model, 'created_at')->textInput() ?>
+
+    <?= $form->field($model, 'updated_at')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ backend/views/product/index.php
@@ -0,0 +1,59 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel backend\models\ProductSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = Yii::t('app', 'Products');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="product-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a(Yii::t('app', 'Create Product'), ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'name',
+            'slug',
+            'short_description',
+            'description:ntext',
+            //'cost_price',
+            //'regular_price',
+            //'sale_price',
+            //'selling_price',
+            //'SKU',
+            //'quantity',
+            //'image',
+            //'images:ntext',
+            //'is_luxury',
+            //'related_product',
+            //'gender',
+            //'trademark_id',
+            //'viewed',
+            //'fake_sold',
+            //'sold',
+            //'status',
+            //'admin_id',
+            //'created_at',
+            //'updated_at',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+
+</div>
